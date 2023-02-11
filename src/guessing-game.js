48bc1e1ae7404f0d248aef16e2ba23862ba4b061
andryelsa <andrejelsakov0@gmail.com>
2023-02-11 10:37:56 +0300

    init: first version

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.minValue = min;
+      this.maxValue = max;
     }
 
     guess() {
-
+      return this.Middle = Math.round((this.minValue + this.maxValue) / 2);
     }
 
     lower() {
-
+      this.maxValue = this.Middle;
     }
 
     greater() {
-
+      this.minValue = this.Middle;
     }
 }
 
